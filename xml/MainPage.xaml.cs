7073482427b01e89ed12d0af8aa5d3cfbd343932
svenschilling <sven.schilling@gmail.com>
2022-03-18 19:19:46 +0100

    Add project files.

---
+++ xml/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace xml;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnClickLoad(object sender, EventArgs e)
+	{
+		StatusMsg.Text = "Stop loading me idiot";
+	}
+	private void OnClickSave(object sender, EventArgs e)
+    {
+		StatusMsg.Text = "Stop saving me idiot";
+    }
+}
+
